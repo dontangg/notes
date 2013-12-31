b19feaf0ab2c43ef40f35afeed2e25068404684e
Don Wilson <robert.don.wilson@gmail.com>
2013-12-31 08:56:03 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# notes
+
+A site to host a competition to guess who likes that song among friends.
+
+## README
+
+This README would normally document whatever steps are necessary to get the
+application up and running.
+
+Things you may want to cover:
+
+* Ruby version
+* System dependencies
+* Configuration
+* Database creation
+* Database initialization
+* How to run the test suite
+* Services (job queues, cache servers, search engines, etc.)
+* Deployment instructions
+* ...
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Notes::Application.config.secret_key_base = '0426056e04b9ecb8f5e18ac6e2fe43f68b046e662e204a1db2ba2022c5521046ad23e3c33d5f6641f0a4b1b6c8e3b774e2d40e719c70a35f8fe3a336a8bbdba7'
