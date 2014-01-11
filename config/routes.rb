b21e7f370412a289d9fed41a89eb81758b49f0a9
Don Wilson <robert.don.wilson@gmail.com>
2014-01-11 12:11:27 -0700

    Add submissions #index, #edit, #update, #destroy

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Notes::Application.routes.draw do
   get "sign_out" => "sessions#destroy", :as => "sign_out"
   get "sign_up" => "users#new", :as => "sign_up"
 
-  root :to => "users#new"
+  root :to => "submissions#index"
 
   resources :users
   resources :sessions
