3f71e00e869f5760e614a6674fb7f98daf449cd5
Don Wilson <robert.don.wilson@gmail.com>
2014-01-13 20:33:49 -0700

    Auto sign-in a newly created user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      # Auto-sign-in the user unless the user is already signed in
+      # (eg. creating an account for someone else)
+      session[:user_id] = @user.id unless current_user
+
       redirect_to root_url, :notice => "Signed up!"
     else
       render "new"
